9f78ab05c0daad37f4a4b52a7a29cb59de746573
Syahrul Anuar <syahrlanuar95@gmail.com>
2023-04-16 01:49:28 +0800

    feat: add defaults

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css';
+</script>
+
+<main class="bg-alice-blue w-full min-h-screen">
+	<slot />
+	<footer />
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				'alice-blue': '#E4EEF1',
+				'green-screen': '#00B140',
+				'eerie-black': '#18231F',
+				'palatinate-blue': '#4A35D4'
+			}
+		}
+	},
+	plugins: []
+};
